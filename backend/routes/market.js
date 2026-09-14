05b9885d9e39a8df540beae92092fa5c133f99e2
emasco12 <74816829+emasco12@users.noreply.github.com>
2026-09-14 11:30:22 +0100

    Connect market route to market service

--- backend/routes/market.js
+++ backend/routes/market.js
@@ -1,16 +1,19 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+
 const { getMarketData } = require("../services/marketService");
 
 router.get("/", async (req, res) => {
   try {
-    const marketData = await getMarketData();
-    res.json(marketData);
+    const data = await getMarketData();
+
+    res.json(data);
   } catch (error) {
     console.error(error);
 
     res.status(500).json({
       success: false,
-      message: "Unable to load market data."
+      message: "Unable to load market data",
     });
   }
 });
