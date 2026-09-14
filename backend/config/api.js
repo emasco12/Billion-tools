fe861abc0368c7f15a97e1e2ddcd74c36865c70c
emasco12 <74816829+emasco12@users.noreply.github.com>
2026-09-14 11:27:24 +0100

    Update api.js

--- backend/config/api.js
+++ backend/config/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TWELVE_DATA_API_KEY: process.env.TWELVE_DATA_API_KEY || "",
-  FINNHUB_API_KEY: process.env.FINNHUB_API_KEY || "",
-  ALPHA_VANTAGE_API_KEY: process.env.ALPHA_VANTAGE_API_KEY || ""
+  TWELVE_DATA_API_KEY: process.env.TWELVE_DATA_API_KEY,
+  FINNHUB_API_KEY: process.env.FINNHUB_API_KEY,
+  ALPHA_VANTAGE_API_KEY: process.env.ALPHA_VANTAGE_API_KEY,
 };
